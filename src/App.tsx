e2749c94c08fdbc31a7a4a320f586d08a1e66a9f
Jmeza90 <jaime.meza.araneda@gmail.com>
2022-06-23 14:26:32 -0400

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './App.css';
+import { Contenido } from './Contenido';
+
+function App() {
+  return(
+    <React.Fragment>
+      <Contenido/>     
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Contenido/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Contenido() {
+    return (
+        <p>Hola Mundo</p>
+        
+    );
+}
+
+export { Contenido }
